91acdb9f9d0345e0c5c6cb4ca17e27c2cf69d0ae
pathikroy86 <pathik.roy86@gmail.com>
2026-06-09 01:46:41 +0600

    ideas page

--- src/app/ideas/page.jsx
+++ src/app/ideas/page.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import IdeaCard from '../components/IdeaCard';
+import { Label, SearchField } from '@heroui/react';
 
-const IdeasPage = () => {
+const IdeasPage = async () => {
+    const res = await fetch('http://localhost:8008/ideas');
+    const ideas = await res.json();
     return (
-        <div>
+        <div className='container mx-auto mt-5'>
+            <h1 className='text-2xl md:text-4xl lg:text-5xl font-semibold mb-4'>All Ideas</h1>
+            <p className='text-gray-400 font-medium mb-6'>Explore and discover amazing startup ideas from the community.</p>
+            <div className='mb-6 flex flex-col md:flex-row'>
+                <div>
+                    <SearchField name="search">
+                        <Label>Search</Label>
+                        <SearchField.Group>
+                            <SearchField.SearchIcon />
+                            <SearchField.Input className="w-[280px]" placeholder="Search..." />
+                            <SearchField.ClearButton />
+                        </SearchField.Group>
+                    </SearchField>
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                {
+                    ideas.map(idea => <IdeaCard key={idea._id} idea={idea}></IdeaCard>)
+                }
+            </div>
 
         </div>
     );
