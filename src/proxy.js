b29cbfe574893abac2f08b44db07c48ea84f5e3d
pathikroy86 <pathik.roy86@gmail.com>
2026-06-12 14:14:18 +0600

    proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,12 +1,17 @@
 import { NextResponse } from 'next/server'
+import { auth } from './lib/auth'
+import { headers } from 'next/headers'
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request) {
-    return NextResponse.redirect(new URL('/signin', request.url))
-}
+export async function proxy(request) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session) {
+        return NextResponse.redirect(new URL('/signin', request.url))
+    }
 
-// Alternatively, you can use a default export:
-// export default function proxy(request) { ... }
+}
 
 export const config = {
     matcher: ['/ideas', '/ideas/:id', '/addideas', '/myideas', '/myinterractions'],
